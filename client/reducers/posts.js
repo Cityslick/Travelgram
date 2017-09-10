db7cb66ea2a88ebef50f85327bad250918d63954
Rajiste Bennin <rajisteb@gmail.com>
2017-09-10 11:04:18 -0400

    increment likes

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -9,9 +9,23 @@
 //In comes both the action (add, delete, etc) and the store/state into the reducer
 //Reducer does [something] and returns a new updated store and then React updates the UI
 
+// Reducers are pure functions, you cannot directly modify external state within a reducer 
+
 function posts(state = [], action) {
-    console.log('the post will change');
-    console.log(state, action);
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :
+        // return updated state
+            console.log('incrementing likes');
+            const i = action.index;
+            return [
+                ...state.slice(0, i),
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1),
+            ]
+        default:
+            return state;
+
+    }
     return state;
 }
 
