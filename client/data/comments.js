30fbb1de9d64f711e8d95ad3de6ebb271bb35bf0
Rajiste Bennin <rajisteb@gmail.com>
2017-09-10 13:43:30 -0400

    comments

--- client/data/comments.js
+++ client/data/comments.js
@@ -1,60 +1,40 @@
 const comments = {
   "BAhvZrRwcfu":[
     {
-      "text":"Totally need to try this.",
+      "text":"Totally need visit Italy someday.",
       "user": "heavymetaladam"
     }
   ],
   "BAcyDyQwcXX":[
     {
-      "text":"Wes. WE should have lunch.",
-      "user": "jdaveknox"
+      "text":"Yes. WE should have lunch.",
+      "user": "kev"
     },
     {
-      "text":"#adults",
-      "user": "jdaveknox"
+      "text":"#sick!",
+      "user": "kev"
     },
     {
-      "text":"@jdaveknox yes!",
-      "user": "wesbos"
+      "text":"@kev hit me up!",
+      "user": "brytiller"
     },
     {
-      "text":"😍 love Hamilton!",
-      "user": "willowtreemegs"
+      "text":"😍 love Portugal!",
+      "user": "willowmegs"
     }
   ],
   "BAPIPRjQce9":[
     {
-      "text":"Those are cute! They're like silver dollar pancakes.",
+      "text":"This is amazing!.",
       "user": "rrsimonsen"
     },
     {
-      "text":"I like baby pancakes but gluten free please!! I'll bring the coffee!! See you in 6 days!!!!!! 😝😛😝♥️",
-      "user": "terzisn"
-    },
-    {
-      "text":"... and apparently growing baby. 👀. Yum.",
-      "user": "henrihelvetica"
-    },
-    {
-      "text":"@wesbos 👍 my daughter is a pancake eating machine!",
+      "text":"you're a traveling machine!",
       "user": "brentoage"
     },
     {
-      "text":"Nice stove!",
+      "text":"Nice beach!",
       "user": "haaps"
-    },
-    {
-      "text":"Genius bottle use! Do you make a single batch of batter or make a lot and freeze it?",
-      "user": "gobananna"
-    },
-    {
-      "text":"@gobananna I just made a batch and put in in a FIFO bottle. Keeps in the fridge for a few days.",
-      "user": "wesbos"
-    },
-    {
-      "text":"@haaps thanks! It's a Jenn air - so nice to cool with!",
-      "user": "wesbos"
     },
     {
       "text":"Where would you go and for how long, if you had location freedom? - Greg 🌎",
@@ -63,51 +43,39 @@ const comments = {
   ],
   "BAF_KY4wcRY":[
     {
-      "text":"Looking great Wes! I'd like to see the other side of the room too.",
+      "text":"Absolutely amazing.",
       "user": "axcdnt"
-    },
-    {
-      "text":"I've never caught your podcast. Have one right? Btw - they don't have a Canary pillow? 😝",
-      "user": "henrihelvetica"
     },
     {
       "text":"Great way to start the year.",
       "user": "pmgllc"
     },
     {
-      "text":"Are there 4k monitors?",
+      "text":"How many steps to the top?",
       "user": "alexbaumgertner"
     },
     {
-      "text":"@axcdnt that is where I put all the junk. I'll have to clean that side too @henrihelvetica no podcast yet! @pmgllc ohh yeah! @alexbaumgertner yep - the main one is 4K - I'm loving it",
-      "user": "wesbos"
-    },
-    {
-      "text":"That chrome pillow. 😉",
+      "text":"That gold statue. 😉",
       "user": "imagesofthisandthat"
     },
     {
-      "text":"@wesbos is that the Dell 4k? The MacBook Pro powers it well? I also have a Retina™ / x1 setup as well. Very handy.",
-      "user": "henrihelvetica"
-    },
-    {
-      "text":"#minimalsetups",
-      "user": "wesbos"
+      "text":"#travel #digitalnomad",
+      "user": "ryan"
     }
   ],
   "_4jHytwcUA":[
     {
-      "text":"that is the sound of success!",
+      "text":"I can hear the waves already!",
       "user": "mdxprograms"
     }
   ],
   "_zbaOlQcbn":[
     {
-      "text":"Did she get to eat her letter?",
+      "text":"Wait, midnight?",
       "user": "pathiebert"
     },
     {
-      "text":"Nope @pathiebert! She has too much teeth now (8) so that would definitely be her first cavity 😉",
+      "text":"@pathiebert that's right! Way, way up north",
       "user": "kaitbos"
     }
   ],
@@ -115,30 +83,26 @@ const comments = {
     {
       "text":"A+",
       "user": "mrjoedee"
-    },
-    {
-      "text":"I recently went to a ramen place in Philly. So amazing!",
-      "user": "jrtashjian"
     }
   ],
   "_ep9kiQcVy":[
     {
-      "text":"All bundled up!  Where ya goin?",
+      "text":"Great view!  How long are you visiting?",
       "user": "sophie_and_sadie"
     }
   ],
   "_XpJcrwcSn":[
     {
-      "text":"Super congrats! That's wicked cool and looks great.",
+      "text":"What a great shot!",
       "user": "pmgllc"
     },
     {
-      "text":"real live paper magazine? woah haha. flex box is awesome though, could rage quit without it",
+      "text":"Since when were you in Japan?!",
       "user": "tjholowaychuk2"
     },
     {
-      "text":"@tjholowaychuk2 haha yes! Old school stylez!",
-      "user": "wesbos"
+      "text":"@tjholowaychuk2 just got here yesterday! ",
+      "user": "potsnpans"
     }
   ],
   "_KnU7MwceA":[
@@ -150,13 +114,13 @@ const comments = {
       "user": "t_volpe"
     },
     {
-      "text":"Nice shot, mister!",
+      "text":"😍",
       "user": "imagesofthisandthat"
     }
   ],
   "_Fq2zmwcaz":[
     {
-      "text":"😍",
+      "text":"Nice shot! ",
       "user": "melsariffodeen"
     },
     {
@@ -166,7 +130,7 @@ const comments = {
   ],
   "_A2r0aQcfD":[
     {
-      "text":"Uhu!",
+      "text":"Have a drink for me waiting, I'm on my way!",
       "user": "lucascaixeta"
     }
   ],
