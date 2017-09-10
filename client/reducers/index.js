a94a3d1d0a703e13a44274fc7f716c4dfdf301b6
Rajiste Bennin <rajisteb@gmail.com>
2017-09-10 10:06:37 -0400

    create redux reducers

---
+++ client/reducers/index.js
@@ -0,0 +1,16 @@
+//Root reducer - each default state has to have a reducer and then each reducer has to be
+//put together as one big, overall root reducer
+
+import { combineReducers } from 'redux';
+import {routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({
+    posts,
+    comments,
+    router: routerReducer
+})
+
+export default rootReducer;
---
+++ client/reducers/posts.js
@@ -0,0 +1,13 @@
+// a reducer takes in two things:
+// 1. the action (what happened)
+// 2. a copy/snapshot of current state
+
+//In comes both the action (add, delete, etc) and the store/state into the reducer
+//Reducer does [something] and returns a new updated store and then react updates the UI
+
+function posts(state = [], action) {
+    console.log(state, action);
+    return state;
+}
+
+export default posts;
